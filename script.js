a4a2650ccbc696858132b359504ac6034c1bff1b
LuiCoro <luiacorona029@icloud.com>
2022-04-08 19:56:45 -0700

    * script.js * + grabs elements from the DOM + Added eventListeners w/ their correct type + basic functions are built to establish building blocks

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // JavaScript Goes Here
+const video = document.getElementById('video');
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
+
+//Play & Pause Video
+function toggleVideoStatus() {
+  return true;
+}
+
+// Update Play/Pause Icon
+function updatePlayIcon(){
+  return true;
+}
+
+// Update Progress & Timestamp
+function updateProgress() {
+  return true;
+}
+
+//Set Video Time to Progress
+function setVideoProgress() {
+  return true;
+}
+
+//Stop Video
+function stopVideo() {
+  return true;
+}
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon)
+video.addEventListener('play', updatePlayIcon)
+video.addEventListener('timeupdate', updateProgress)
+
+play.addEventListener('click', toggleVideoStatus)
+
+stop.addEventListener('click', stopVideo)
+
+progress.addEventListener('change', setVideoProgress)
